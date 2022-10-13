d321a44fdbedfc33feb165ee33a8561640065a57
Trần Hải <haitran1256@gmail.com>
2022-10-13 15:07:17 +0700

    add new project book

---
+++ quanlythuvien.sql
@@ -0,0 +1,15 @@
+create database Quanlythuvien;
+use Quanlythuvien;
+
+create table sach(
+	Ma int primary key,
+    Ten varchar(40) not null,
+    Tacgia varchar(40) not null,
+    Nxb varchar(40) not null,
+    Theloai varchar(30),
+    Ngayph date 
+    );
+drop table sach
+select * from sach
+    
+    
